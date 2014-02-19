be917a37d814437863f5f3ba0ecf6710ace58650
Arun Bhati <arnbhati@gmail.com>
2014-02-19 16:58:40 +0530

    tftp client complete

--- tftp_client.cpp
+++ tftp_client.cpp
@@ -6,9 +6,12 @@
 #include<cstring>
 #include<string>
 #include<netdb.h>
+#include<iostream>
+#include<fstream>
 #include<sys/socket.h>
 #include<sys/types.h>
 #include<netinet/in.h>
+#include<sys/poll.h>
 
 using namespace std ;
 
@@ -17,21 +20,23 @@ using namespace std ;
 #define DATA  3
 #define ACK   4
 #define ERROR 5
-#define max_size 1000024
+#define MAX_SIZE 1000024
 
 // global variable
 string output_buffer ;
-int socket_id , bind_id , seraddr_len ;
+int socket_id , bind_id , seraddr_len , poll_count , poll_break ;
 int packet_len , packet_count , packet_cur_size , fbytes, sbytes , charcount ;
-char packet_buffer[max_size] ;
-const string mode = "netascii" ;
+char packet_buffer[MAX_SIZE] ;
+const string mode = "octet" ;
 struct hostent *hp ;
 struct sockaddr_in myaddr , seraddr;
+struct pollfd ufds ;
 bool last_packet ;
 char host[500] ;
-
+ofstream myfile ;
 void print_error(int i) ;
-string Fill_buffer(string inputfile) ;
+bool Fill_buffer(string inputfile) ;
+string CONN_ERROR = "problem in sending data to server try after some time" ;
 
 void conn_start()
 {
@@ -52,6 +57,11 @@ void conn_start()
     seraddr.sin_family = AF_INET;
     seraddr.sin_port = htons(69);
     memcpy((void *)&seraddr.sin_addr, hp->h_addr_list[0], hp->h_length);
+
+
+    // update values for polling
+    ufds.fd = socket_id ;
+    ufds.events = POLLIN ; // check for normal or out-of-band
 }
 
 void conn_close()
@@ -59,7 +69,7 @@ void conn_close()
     close(socket_id) ;
 }
 
-void read_request(string FileName)
+bool read_request(string FileName)
 {
     packet_count = 0 ;
     packet_len = 2 ;
@@ -79,27 +89,54 @@ void read_request(string FileName)
     packet_buffer[packet_len++] = '\0' ;
 
 
-    sendto(socket_id, packet_buffer, packet_len, 0 , (struct sockaddr *)&seraddr, sizeof(seraddr)) ;
-    return ;
+    if(sendto(socket_id, packet_buffer, packet_len, 0 , (struct sockaddr *)&seraddr, sizeof(seraddr))==-1)
+    {
+        cout << "Unable to Send Read Request Resending..." << endl  ;
+        return false ;
+    }
+    cout << "Read Request send to server for file name =" << FileName << endl  ;
+    return true ;
 }
 
 bool get_data()
 {
-        bool flag = true ;
+        int rv  ;
         seraddr_len = sizeof(seraddr) ;
-        int retsize = recvfrom(socket_id, packet_buffer, max_size , 0, (sockaddr*) &seraddr, (socklen_t *)&seraddr_len);
+        poll_break = 0 ;
+        poll_count = 5 ;
+        while(poll_count!=0)
+        {
+            rv = poll(&ufds, 1 , 3500);
+            if(rv > 0)
+                break ;
+
+            if (rv == -1) {
+                perror("poll"); // error occurred in poll()
+                printf("poll error \n");
+            } else if (rv == 0) {
+                printf("Timeout occurred!  No data after 3.5 seconds.\n");
+            }
+            poll_count-- ;
+        }
+        if(!poll_count)
+        {
+            poll_break = 1 ;
+            return true ;
+        }
+        if (rv == -1) {
+            perror("poll"); // error occurred in poll()
+            printf("poll error \n");
+            return true ;
+        } else if (rv == 0) {
+            printf("Timeout occurred!  No data after 3.5 seconds.\n");
+            return true ;
+        }
+        int retsize = recvfrom(socket_id, packet_buffer, MAX_SIZE , 0, (sockaddr*) &seraddr, (socklen_t *)&seraddr_len);
         if(retsize == -1)
         {
             cout << "\nRecv Error : " ;
 
-     /*       cout << "\nRecv Error : " << WSAGetLastError();
-
-            if (WSAGetLastError() == WSAEWOULDBLOCK || WSAGetLastError() == 0)
-            {
-                return "";
-            }
-    */
-            return flag ;
+            return true ;
         }
         charcount = 0 ;
         fbytes = (int)packet_buffer[0] ;
@@ -115,11 +152,14 @@ bool get_data()
                 packet_count++ ;
 
                 for(int i=2;i<retsize;i++,charcount++)
+                {
+                    myfile << packet_buffer[i] ;
                     cout << packet_buffer[i]  ;
-
+                }
                 if(retsize-4 < 512)
                 return false ;
 
+                return true ;
             }else
                 cout << sbytes << " IS A DUPLICATE PACKET " << endl ;
             return true ;
@@ -129,12 +169,13 @@ bool get_data()
             sbytes = (int)packet_buffer[3] ;
             sbytes = sbytes + ( fbytes << 8 ) ;
             print_error(sbytes) ;
+            poll_break = 1 ;
             return false ;
         }
 
 }
 
-void send_ack()
+bool send_ack()
 {
     int temp = packet_count ;
     packet_len = 4 ;
@@ -143,12 +184,16 @@ void send_ack()
     packet_buffer[3] = '\0' + (temp & 255) ;
     temp = temp >> 8 ;
     packet_buffer[2] = '\0' + (temp & 255) ;
-    sendto(socket_id, packet_buffer, packet_len, 0 , (struct sockaddr *)&seraddr, sizeof(seraddr)) ;
-    return ;
+    if(sendto(socket_id, packet_buffer, packet_len, 0 , (struct sockaddr *)&seraddr, sizeof(seraddr))==-1)
+    {
+        cout << "Unable to send acknoledge Resending..." << endl ;
+        return false ;
+    }
+    return true ;
 }
 
 
-void write_request(string FileName)
+bool write_request(string FileName)
 {
     packet_count = 0 ;
     packet_len = 2 ;
@@ -168,26 +213,57 @@ void write_request(string FileName)
     }
     packet_buffer[packet_len++] = '\0' ;
 
-    sendto(socket_id, packet_buffer, packet_len, 0 , (struct sockaddr *)&seraddr, sizeof(seraddr)) ;
-    return ;
+    if(sendto(socket_id, packet_buffer, packet_len, 0 , (struct sockaddr *)&seraddr, sizeof(seraddr))==-1)
+    {
+        cout << "Unable to Send Write Request Resending..." << endl  ;
+        return false ;
+    }
+    cout << "Write Request send to server for file name =" << FileName << endl  ;
+    return true ;
 }
 
 
 bool get_ack()
 {
     seraddr_len = sizeof(seraddr) ;
-    int retsize = recvfrom(socket_id, packet_buffer, max_size , 0, (sockaddr*) &seraddr, (socklen_t *)&seraddr_len);
+    int rv  ;
+
+    poll_break = 0 ;
+    poll_count = 5 ;
+    while(poll_count!=0)
+    {
+        rv = poll(&ufds, 1 , 3500);
+        if(rv > 0)
+            break ;
+
+        if (rv == -1) {
+            perror("poll"); // error occurred in poll()
+            printf("poll error \n");
+        } else if (rv == 0) {
+            printf("Timeout occurred!  No data after 3.5 seconds.\n");
+        }
+        poll_count-- ;
+    }
+    if(!poll_count)
+    {
+        poll_break = 1 ;
+        return false ;
+    }
+    if (rv == -1) {
+        perror("poll"); // error occurred in poll()
+        printf("poll error \n");
+        return false ;
+    } else if (rv == 0) {
+        printf("Timeout occurred!  No data after 3.5 seconds.\n");
+        return false ;
+    }
+
+
+    int retsize = recvfrom(socket_id, packet_buffer, MAX_SIZE , 0, (sockaddr*) &seraddr, (socklen_t *)&seraddr_len);
+
     if(retsize == -1)
     {
         cout << "\nRecv Error : " ;
-
- /*       cout << "\nRecv Error : " << WSAGetLastError();
-
-        if (WSAGetLastError() == WSAEWOULDBLOCK || WSAGetLastError() == 0)
-        {
-            return "";
-        }
-*/
         return false ;
     }
     fbytes = (int)packet_buffer[0] ;
@@ -211,6 +287,7 @@ bool get_ack()
         sbytes = (int)packet_buffer[3] ;
         sbytes = sbytes + ( fbytes << 8 ) ;
         print_error(sbytes) ;
+        poll_break = 1 ;
         return false ;
 
     }else
@@ -220,7 +297,7 @@ bool get_ack()
 }
 
 
-void send_data(bool got_ack)
+bool send_data(bool got_ack)
 {
     int temp ;
     packet_len = 4 ;
@@ -239,10 +316,14 @@ void send_data(bool got_ack)
         {
             packet_buffer[packet_len] = output_buffer[i] ;
         }
-        sendto(socket_id, packet_buffer, packet_len, 0 , (struct sockaddr *)&seraddr, sizeof(seraddr)) ;
+        if(sendto(socket_id, packet_buffer, packet_len, 0 , (struct sockaddr *)&seraddr, sizeof(seraddr))==-1)
+        {
+            cout << "Unable to send data Resending..." << endl ;
+            return false ;
+        }
         if(count < 512 && i >= output_buffer.length())
             last_packet = true ;
-        return ;
+        return true ;
     }
     temp = packet_count ;
     packet_buffer[3] = '\0' + (temp & 255) ;
@@ -257,8 +338,12 @@ void send_data(bool got_ack)
     {
         packet_buffer[packet_len] = output_buffer[i] ;
     }
-    sendto(socket_id, packet_buffer, packet_len, 0 , (struct sockaddr *)&seraddr, sizeof(seraddr)) ;
-    return ;
+    if(sendto(socket_id, packet_buffer, packet_len, 0 , (struct sockaddr *)&seraddr, sizeof(seraddr))==-1)
+    {
+        cout << "Unable to send data Resending..." << endl ;
+        return false ;
+    }
+    return true ;
 }
 
 
@@ -266,8 +351,11 @@ void send_data(bool got_ack)
 
 int main(int argc, char* argv[])
 {
-    bool got_ack ;
-    int i ;
+    bool got_ack , flag ;
+    int i , count ;
+
+
+
     string FileName,FileName1 ;
     strcpy(host,"localhost") ;
     conn_start() ;
@@ -275,14 +363,13 @@ int main(int argc, char* argv[])
     while(1)
     {
         cout << endl ;
-        cout << "Enter 1:   If you want to change ServerName (Current Surver is \"Localhost\")." << endl ;
+        cout << "Enter 1:   If you want to change ServerName (Current Surver is \""<< host << "\")." << endl ;
         cout << "Enter 2:   If you want to read file from server." << endl ;
         cout << "Enter 3:   If you want to write file to server." << endl << endl ;
         cout << "Enter Something else to EXIT." << endl ;
         cin >> i ;
 
-        if(i==1)
-        {
+        if(i==1){
             cout << "Enter Host Name : " ;
             scanf("%s",host) ;
             cout << endl ;
@@ -291,32 +378,123 @@ int main(int argc, char* argv[])
 
         }else if(i==2)
         {
+            conn_close() ;
+            conn_start() ;
+
             cout << "Enter File Name(without space) : " ;
             cin >> FileName ;
             cout << endl ;
-            read_request(FileName) ;
+
+            myfile.open(FileName.c_str()) ;
+
+            count = 15 ;
+            flag = read_request(FileName) ;
+            while(!flag && count)
+            {
+                flag = read_request(FileName) ;
+                count-- ;
+            }
+            if(count==0)
+            {
+                cout << CONN_ERROR << endl ;
+                continue ;
+            }
+            poll_break =  0 ;
             while(get_data())
             {
-                send_ack() ;
+                if(poll_break)
+                {
+                    cout << CONN_ERROR << endl ;
+                    break ;
+                }
+
+                count = 15 ;
+                flag = send_ack() ;
+                while(!flag && count)
+                {
+                    flag = send_ack() ;
+                    count-- ;
+                }
+                if(count==0)
+                {
+                    cout << CONN_ERROR << endl ;
+                    continue ;
+                }
             }
+            if(poll_break)
+            {
+                myfile.close() ;
+                continue ;
+            }
+            count = 15 ;
+            flag = send_ack() ;
+            while(!flag && count)
+            {
+                flag = send_ack() ;
+                count-- ;
+            }
+            if(count==0)
+            {
+                cout << CONN_ERROR << endl ;
+                continue ;
+            }
+            myfile.close() ;
 
         }else if(i==3)
         {
-            cout << "Enter File Name(Give Complete Path if file is not in home folder) : " ;
+            conn_close() ;
+            conn_start() ;
+
+            cout << "Enter File Name(File should in current folder) : " ;
             cin >> FileName ;
             cout << endl ;
-            cout << "Enter File Name(This name will be shown on server) : " ;
-            cin >> FileName1 ;
-            Fill_buffer(FileName) ;
-            write_request(FileName1) ;
+
+            if(!Fill_buffer(FileName))
+                continue ;
+
+
+            count = 15 ;
+            flag = write_request(FileName) ;
+            while(!flag && count)
+            {
+                flag = write_request(FileName) ;
+                count-- ;
+            }
+            if(count==0)
+            {
+                cout << CONN_ERROR << endl ;
+                continue ;
+            }
+
             last_packet = false ;
+            poll_break =  0 ;
             while(1)
             {
                 got_ack = get_ack() ;
+
+                if(poll_break)
+                {
+                    cout << CONN_ERROR << endl ;
+                    break ;
+                }
+
                 if(got_ack && last_packet)
                     break ;
-                send_data(got_ack) ;
+
+                count = 15 ;
+                flag = send_data(got_ack) ;
+                while(!flag && count)
+                {
+                    flag = send_data(got_ack) ;
+                    count-- ;
+                }
+                if(count==0)
+                {
+                    cout << CONN_ERROR << endl ;
+                    continue ;
+                }
             }
+
         }else
         {
             break ;
@@ -328,15 +506,17 @@ int main(int argc, char* argv[])
 
 
 
-string Fill_buffer(string inputfile)
+bool Fill_buffer(string inputfile)
 {
   ifstream fd (inputfile.c_str());
   if (fd.is_open())
   {
     getline (fd, output_buffer , '\0') ;
     fd.close();
+    return true ;
   }
-  else cout << "Unable to open file" <<  endl ;
+  else cout << "Unable to open file. \n  file should be in same directory." <<  endl ;
+  return false ;
 }
 
 
